5754d4c2ca76691ac27b95d9deb53c0f811f89b5
skyshaver <skyshaver@me.com>
2019-03-07 18:37:12 -0500

    BT05 - create tank and tankplayercontroller cpp classes and reparent matching blueprints in ue editor, MAKE SURE YOU'RE EDITING THE RIGHT BLUEPRINT WHEN YOU REPARENT, created a pointer to the possessed pawn and logged it. Pointer safety with const functions?

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();	// insaures we're overriding beginPlay from the super(parent) class
+
+	ATank* ControlledTank = GetControlledTank();
+
+	if (!ControlledTank) { UE_LOG(LogTemp, Warning, TEXT("Failed to capture pawn")); }
+	else { UE_LOG(LogTemp, Warning, TEXT("Pawn %s is under control"), *ControlledTank->GetName()); }
+
+	// UE_LOG(LogTemp, Warning, TEXT("Player Controller Begin Play"));
+}
+
+ATank*  ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
